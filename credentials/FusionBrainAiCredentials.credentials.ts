63bf492710e1ae026a1ec443715f9fc3ac87e18a
Franz Haberfellner <Franz.Haberfellner@karriere.at>
2024-03-01 12:53:36 +0100

    Credentials
    Signed-off-by: Franz Haberfellner <Franz.Haberfellner@karriere.at>

---
+++ credentials/FusionBrainAiCredentials.credentials.ts
@@ -0,0 +1,46 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class FusionBrainAiCredentials implements ICredentialType {
+	name = 'hapheusFusionBrainAiApiCredentials';
+	displayName = 'fusionbrain.ai API';
+	documentationUrl = 'https://fusionbrain.ai/docs/en/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Api Key',
+			name: 'apiKey',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Secret Key',
+			name: 'secretKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				"X-Key": '={{"Key " + $credentials.apiKey}}',
+				"X-Secret": '={{"Secret " + $credentials.secretKey}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api-key.fusionbrain.ai/key/api/v1/',
+			url: 'models',
+		},
+	};
+}
